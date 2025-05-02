6cf9acc8ec0d2798f4f580813d3daade91c1e798
JERSONCALDUCHO <jerson.11106@gmail.com>
2025-05-02 10:18:23 -0500

    implementacion de python

--- src/app.py
+++ src/app.py
@@ -7,28 +7,28 @@ if __name__ == '__main__':
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',titulo='Bienvenido a la aplicación de facturación')
 
 @app.route('/lista_productos')
 def lista_productos():
-    return render_template('lista_productos.html')
+    return render_template('lista_productos.html',titulo='Listado de Productos')
 
 @app.route('/inicio_sesion')
 def inicio_sesion():
-    return render_template('inicio_sesion.html')
+    return render_template('inicio_sesion.html',titulo='Inicio sesion')
 
 @app.route('/formulario_usuario')
 def formulario_usuario():
-    return render_template('formulario_usuario.html')
+    return render_template('formulario_usuario.html',titulo='Crear Usuario')
 
 @app.route('/formulario_producto')
 def formulario_producto():
-    return render_template('formulario_producto.html')
+    return render_template('formulario_producto.html',titulo='Crear Producto')
 
 @app.route('/formulario_factura')
 def formulario_factura():
-    return render_template('formulario_factura.html')
+    return render_template('formulario_factura.html',titulo='Generar factura')
 
 @app.route('/formulario_cliente')
 def formulario_clientedex():
-    return render_template('formulario_cliente.html')
+    return render_template('formulario_cliente.html',titulo='Crear Cliente')
